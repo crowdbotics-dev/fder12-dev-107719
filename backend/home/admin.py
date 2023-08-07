b4f3acb93066d617092302c9833940483fc6cf1f
crowdbotics-dev <engineering@crowdbotics.com>
2023-08-07 06:41:06 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ASdsf
+admin.site.register(ASdsf)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class ASdsf(models.Model):
+    'Generated Model'
+    jdjd = models.BigIntegerField()
+    ddj = models.BigIntegerField()
